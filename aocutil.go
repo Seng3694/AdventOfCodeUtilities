61705bcc3237c2860d54514a0f0ea0a522fb3bcb
Seng <engelhardt_stefan@gmx.de>
2022-11-27 10:49:20 +0100

    changed timing method to a high precision timer

--- aocutil.go
+++ aocutil.go
@@ -3,12 +3,14 @@ package aocutil
 import (
 	"fmt"
 	"time"
+
+	"github.com/loov/hrtime"
 )
 
-var startTime time.Time = time.Now()
+var startTime time.Duration = hrtime.Now()
 
 func AOCFinish[T any](parts ...T) {
-	elapsed := time.Since(startTime)
+	elapsed := hrtime.Since(startTime)
 	fmt.Println("Solutions:")
 	for i, p := range parts {
 		fmt.Printf(" Part %v: '%v'\n", i, p)
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Seng3694/AdventOfCodeUtilities
 
 go 1.19
 
-require golang.org/x/exp v0.0.0-20221114191408-850992195362
+require (
+	github.com/loov/hrtime v1.0.3
+	golang.org/x/exp v0.0.0-20221114191408-850992195362
+)
